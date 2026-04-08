995441206e2759c2a5ec1e629fc35d92b85aaf03
RajuManur143 <rajumannur18@gmail.com>
2026-04-08 23:19:41 +0530

    Hello java

---
+++ DAY1/Hello.java
@@ -0,0 +1,14 @@
+// Class definition for Hello program
+class Hello{
+    // Main method - entry point of the Java application
+    public static void main(String[] args) {
+
+        // Print the first message to console
+        System.out.println("Hello Java");
+        
+        // Print the college name to console
+        System.out.println("Government Engineering College");
+    
+    }
+
+}
