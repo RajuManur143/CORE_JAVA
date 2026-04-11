bf5a24f877fa9de3bf1c55aa6466a18d93e19c29
RajuManur143 <rajumannur18@gmail.com>
2026-04-11 19:09:41 +0530

    Assignment

---
+++ DAY4/assignment.java
@@ -0,0 +1,108 @@
+package DAY4;
+
+public class assignment {
+
+    // Employee details
+    static String emp_name = "RAJU";
+    static int emp_id = 100;
+    static double emp_salary = 45000;
+
+    // College detailst
+    static String college_name = "GOVT ENG";
+    static char college_grade = 'A';
+    static int college_fees = 35000;
+
+    // Home detais
+    static String home_name = "MR manur farm house";
+    static int home_cost = 400000;
+    static String home_color = "white";
+
+    // Tv details
+    static String tv_name = "LED tv";
+    static int tv_cost = 35000;
+    static String tv_model = "LED tv model";
+
+    // Mobile detais
+    static String mobile_name = "OOP A15";
+    static int mobile_cost = 9000;
+    static String mobile_type = "Water prof";
+
+    // Car details
+    static String car_name = "BMW";
+    static int car_cost = 40000;
+    static String car_model = "MP4 model";
+
+    // cloths details
+    static String cloths_name = "RM%";
+    static int cloths_cost = 4500;
+    static String cloths_brand = "BRand hi";
+
+    // Laptop details
+    static String lap_name = "HP";
+    static int lap_cost = 50000;
+    static String lap_grade = "First grade";
+
+    // bike details
+    static String bike_name = "HF delux";
+    static int bike_cost = 60000;
+    static String bike_max_sped = "120 km/sec";
+
+    // Course details
+    static String course_name = "Computer science and Engineering ";
+    static int course_fees = 34000;
+    static int exam_course_fees = 1680;
+
+    public static void main(String[] args) {
+
+        // Employee details print
+        System.out.println("Employee name = "+emp_name);
+        System.out.println("Employee id = "+emp_id);
+        System.out.println("Employee salary = "+emp_salary);
+
+        // Print college details
+        System.out.println("College name = "+college_name);
+        System.out.println("College grade = "+college_grade);
+        System.out.println("College fees = "+college_fees);
+
+        // Home details print
+        System.out.println("Home name is ="+home_name);
+        System.out.println("Home cost is = "+home_cost);
+        System.out.println("Home color is = "+home_color);
+
+        // Tv details printing
+        System.out.println("TV name is = "+tv_name);
+        System.out.println("TV cost is = "+tv_cost);
+        System.out.println("TV model is = "+tv_model);
+
+        // Mobile details
+        System.out.println("Mobile name = "+mobile_name);
+        System.out.println("Mobile cost = "+mobile_cost);
+        System.out.println("Mobile type = "+mobile_type);
+
+        // Car details printing
+        System.out.println("Car name = "+car_name);
+        System.out.println("Car cost = "+car_cost);
+        System.out.println("Car model = "+car_model);
+
+        // Cloths details printing
+        System.out.println("cloths name = "+cloths_name);
+        System.out.println("Cloths cost is = "+cloths_cost);
+        System.out.println("Cloths brand is = "+cloths_brand);
+
+        // Laptop detais printing
+        System.out.println("Laptop nme = "+lap_name);
+        System.out.println("Laptop cost is = "+lap_cost);
+        System.out.println("Latop grade is = "+lap_grade);
+
+        // Bike details
+        System.out.println("Bike name = "+bike_name);
+        System.out.println("Bike cost is = "+bike_cost);
+        System.out.println("Bike speed running is = "+bike_max_sped);
+
+        // Course details
+        System.out.println("Course name = "+course_name);
+        System.out.println("Course cost is = "+course_fees);
+        System.out.println("Course exam fees = "+exam_course_fees);
+
+    }
+}
---
+++ DAY4/global_variables.java
@@ -0,0 +1,19 @@
+package DAY4;
+
+public class global_variables {
+
+
+    // Global variables
+    static String name = "RAJU";
+    static int id = 36;
+    static char section = 'A';
+
+    public static void main(String[] args) {
+
+        System.err.println("student name is = "+name);
+        System.out.println("Student identity is = "+id);
+        System.out.println("Student section is = "+section);
+
+    }
+    
+}
