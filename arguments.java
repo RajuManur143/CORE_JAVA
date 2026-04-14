f9b9e49dea5199d28f076c1273bb851fdbb1e93a
RajuManur143 <rajumannur18@gmail.com>
2026-04-14 18:33:09 +0530

    core java

---
+++ arguments.java
@@ -0,0 +1,12 @@
+public class arguments {
+
+    static void add(int a , int b) {
+
+        int sum = a +  b;
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        add(10,20);
+    }
+}
