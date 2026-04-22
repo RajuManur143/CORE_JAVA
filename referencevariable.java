d585116780a22f8ac38c4bd8942213d8180187b7
RajuManur143 <rajumannur18@gmail.com>
2026-04-22 23:12:45 +0530

    core java

---
+++ referencevariable.java
@@ -0,0 +1,13 @@
+public class referencevariable {
+
+     int a  = 10;
+    
+    public static void main(String[] args) {
+
+        // System.out.println(a);
+
+        referencevariable res = new referencevariable();
+        System.out.println(res.a);
+        
+    }
+}
